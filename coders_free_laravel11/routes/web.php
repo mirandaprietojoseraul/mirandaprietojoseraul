613fb09af7055f38a89771be4dcb7d27d3f01bed
Jose Raul Miranda Prieto <mirandaprietojoseraul@gmail.com>
2024-08-01 05:21:59 -0400

    Making progress with Codersfree Course

--- coders_free_laravel11/routes/web.php
+++ coders_free_laravel11/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+
+
+// Route::get('/posts/{post}/{category}', function($post, $category){
+//     return "Aquí se mostrará {$post} de la categoría {$category}";
+//     });
+//GET
+//POST
+//PUT
+//PATCH
+//DELETE
